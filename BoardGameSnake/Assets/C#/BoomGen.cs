dd994377d4491bc681cdd681dd9ebc97e02870c3
DESKTOP-CHAS\Chas <mmm998520@gmail.com>
2020-08-26 16:44:38 +0800

    製作炸彈生成器(尾巴出BUG，collider不會收回)

---
+++ BoardGameSnake/Assets/C#/BoomGen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.Board.Game.Snake
+{
+    public class BoomGen : MonoBehaviour
+    {
+        float timer = 0;
+        float timerStoper;
+        public GameObject boomItem;
+
+        void Start()
+        {
+            timerStoper = Random.Range(7.0f, 10.0f);
+        }
+
+        void Update()
+        {
+            if (TrailManager.less)
+            {
+                if ((timer+=Time.deltaTime) > timerStoper)
+                {
+                    Instantiate(boomItem, new Vector3(Random.Range(-2.0f,2.0f), Random.Range(-3.0f,5.0f), 0), Quaternion.identity, transform).name = "boom(item)";
+                    timer = 0;
+                    timerStoper = Random.Range(7.0f, 10.0f);
+                }
+            }
+        }
+    }
+}
---
+++ BoardGameSnake/Assets/C#/PlayerManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+namespace com.Board.Game.Snake
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        public TimeManager timeManager;
+        Vector2[] real = new Vector2[0];//短的(真的能碰到
+        public Vector2[] fake = new Vector2[1] {Vector2.zero};//長的
+        public GameObject hurt;
+        bool haveBoom = false;
+
+        void OnMouseDrag()
+        {
+            if (timeManager.dis > 0)
+            {
+                Vector3 mouse_p = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+                mouse_p.z = transform.position.z;
+                if (Vector3.Distance(mouse_p, transform.position)>0.05f)
+                {
+                    timeManager.lessDis(0.1f);
+                    Vector2[] tempF = new Vector2[fake.Length + 1];
+                    for (int i = 0; i < fake.Length; i++)
+                    {
+                        tempF[i] = fake[i];
+                    }
+                    tempF[fake.Length] = mouse_p;
+                    if (fake.Length <= 1)
+                    {
+                        tempF[0] = transform.position;
+                    }
+                    fake = tempF;
+                    int len = 8;
+                    if (fake.Length > len)
+                    {
+                        real = new Vector2[fake.Length - len];
+                        for(int i = 0; i < real.Length; i++)
+                        {
+                            real[i] = fake[i];
+                        }
+                    }
+                    EdgeCollider2D edgeCollider2D = transform.parent.GetComponent<EdgeCollider2D>();
+                    edgeCollider2D.points = real;
+
+                    LineRenderer lineRenderer = transform.parent.GetComponent<LineRenderer>();
+                    if(lineRenderer.positionCount++ <= 1)
+                    {
+                        lineRenderer.SetPosition(0, transform.position);
+                    }
+                    lineRenderer.SetPosition(lineRenderer.positionCount - 1, mouse_p);
+
+                    transform.position = mouse_p;
+                }
+            }
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            print(name);
+            StartCoroutine("Hurt");
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if(other.name == "boom(item)" && !haveBoom)
+            {
+                Destroy(other.gameObject);
+                haveBoom = true;
+            }
+        }
+
+        WaitForSeconds wait = new WaitForSeconds(0.5f);
+        IEnumerator Hurt()
+        {
+            hurt.SetActive(true);
+            yield return wait;
+            hurt.SetActive(false);
+        }
+    }
+}
+
